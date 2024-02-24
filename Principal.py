de1e2c35a77f726e1ec4e5f26d01d8d060214b29
wlacraf <160874684+wlacraf@users.noreply.github.com>
2024-02-24 01:46:38 -0300

    Add files via upload

---
+++ Principal.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Combate a Arbovirose")
+
+with st.container():
+    st.subheader("PMAC - Arbovirose")
+    st.title("Mapeamento Aéreo ")
+    st.write(':blue[Desenvolvido por Eng Wagner Cunha]')
+    st.write("Geolocalização de Piscinas, Caixas dáguas e Hidromassagens descobertas")
---
+++ pages/Suspeito.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+from streamlit_folium import folium_static
+import folium
+
+
+# Função para carregar dados do CSV
+# Função para carregar dados do CSV e filtrar
+def load_data(file_path):
+    # Carrega o DataFrame do arquivo CSV
+    df = pd.read_csv(file_path)
+
+    # Filtra o DataFrame para manter apenas as linhas onde Suspeito=0
+    filtered_df = df[df['Suspeito'] == 1]
+
+    return filtered_df
+
+
+st.title("Visualizador de Mapa com Coordenadas Geográficas dos Pontos suspeitos")
+file_path = 'pages/piscinas.csv'
+df = load_data(file_path)
+
+if not df.empty:
+    # Inicia o mapa
+    m = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=16)
+
+    # Adicionando um tileset padrão e alternativos
+    folium.TileLayer('OpenStreetMap').add_to(m)
+
+
+    # Adicionando Esri Satellite com URL específica
+    esri_satellite_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+    folium.TileLayer(
+        tiles=esri_satellite_url,
+        attr='Esri',
+        name='Esri Satellite',
+        overlay=False,
+        control=True,
+    ).add_to(m)
+
+    folium.LayerControl().add_to(m)  # Permite ao usuário escolher entre os estilos de mapa
+
+    # Adicionando marcadores com popup
+    for idx, row in df.iterrows():
+        folium.Marker(
+            location=[row['LAT'], row['LON']],
+            popup=f"ID: {row['id_campo']}",  # Supondo que 'id' seja o nome da coluna com a informação desejada
+            icon=folium.Icon(color='red')
+        ).add_to(m)
+
+    folium_static(m)
