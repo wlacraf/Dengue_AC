0756b954d7173bbbf823df581f8f57568ed83ef9
wlacraf <160874684+wlacraf@users.noreply.github.com>
2024-02-27 07:18:46 -0300

    Add files via upload

--- pages/2_❓_Suspeito.py
+++ pages/2_❓_Suspeito.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_folium import folium_static
 import folium
+import base64
 
 # Função para carregar dados do CSV
 # Função para carregar dados do CSV e filtrar
@@ -42,8 +43,16 @@ if not df.empty:
 
     # Adicionando marcadores com popup
     for idx, row in df.iterrows():
-        icon_color = 'green' if  row['status'] else 'red'
-        folium.Marker(location=[row['LAT'], row['LON']], popup=f"ID: {row['id_campo']}",
+        icon_color = 'green' if row['status'] else 'red'
+        # Construir o caminho da imagem usando o id_campo. Ajuste o caminho e a extensão conforme necessário.
+        image_path = f"imagens/{row['id_campo']}.jpg"
+        # Usar o método de incorporação de imagem base64 para inline das imagens
+        encoded_image = base64.b64encode(open(image_path, 'rb').read()).decode()
+        image_html = f"<img src='data:image/jpeg;base64,{encoded_image}' width='200' height='200'><br>ID: {row['id_campo']}"
+        iframe = folium.IFrame(image_html, width=220, height=220)
+        popup = folium.Popup(iframe, max_width=2650)
+        folium.Marker(location=[row['LAT'], row['LON']],
+                      popup=popup,
                       icon=folium.Icon(color=icon_color)).add_to(m)
 
     with st.container():
